13b095225aa50f756c6fe6fcf0bd0db9edda81fe
EricMalpass <ericmalpass@gmail.com>
2021-02-24 14:56:35 -0800

    update

--- homework.js
+++ homework.js
@@ -1,7 +1,11 @@
 var start = document.getElementById("start");
 var quiz = document.getElementById("quiz");
-var qImg = document.getElementById("questionImage");
+var qImg = document.getElementById("qImg");
 var question = document.getElementById("question");
+var counter = document.getElementById("counter");
+var timeGauge = document.getElementById("timeGauge")
+var progress = document.getElementById("progress")
+var scoreContainer = document.getElementById("scoreContainer")
 
 
 var choiceA = document.getElementById("A");
@@ -16,20 +20,95 @@ let questions = [
         choiceA : "Absolutely",
         choiceB : "No",
         choiceC : "Sort of",
-        choiceD : "Are you crazy"
-        correct : "A"
+        choiceD : "Are you crazy",
+        correct : "A",
+    },{
+        question : "Question2?",
+        imgSrc : "questionimage.png",
+        choiceA : "NO",
+        choiceB : "No",
+        choiceC : "Correct",
+        choiceD : "NO",
+        correct : "B",
     }
 ]
 
-let lastQuestionIndex = questions - 1;
-let runningQuestionIndex = 0;
+var lastQuestionIndex = questions.length - 1;
+var runningQuestionIndex = 0;
 
 function renderQuestion(){
     let q = questions[runningQuestionIndex];
-    qImg.innerHTML = <img src="+ q.imgSrc + ">";
+    qImg.innerHTML = `<img src=${q.imgSrc}>`;
     question.innerHTML = "<p>" + q.question + "</p>";
     choiceA.innerHTML =  q.choiceA;
     choiceA.innerHTML =  q.choiceB;
     choiceA.innerHTML =  q.choiceC;
     choiceA.innerHTML =  q.choiceD;
     }
+start.style.display = "none";
+renderQuestion();
+quiz.style.display = "block";
+
+/*
+var questionTime = 10;
+var gaugeWidth = 150;
+let count = 0;
+var gaugeProgressUnit = gaugeWidth/questionTime;
+let timer = setInterval(counterRender, 1000);
+
+
+functioncounterRender(){
+    if(count<=questionTime){
+        count.innerHTML = count;
+        timeGauge.syle.width = gaugeProgress * count + "px";
+        count++;
+    }
+}else{
+    count = 0;
+    if( runningQuestionIndex<lastquestion){
+        runningQuestionIndex++;
+        questionRender();
+    }else{clearInterval(timer);
+    scoreRender();
+}
+
+}
+
+let score = 0;
+function checkAnswer(answer){
+    if(questions[runningQuestionIndex].correct==answer){
+        score++;
+        answerIsCorrect();
+    }else{
+        answerIswrong();
+
+    }
+    if(runningQuestionIndex< lastQuestionIndex){
+        count = 0;
+        runningQuestionIndex++;
+        question();
+        }else{
+            clearInterval(timer);
+            scoreRender();
+        }
+}
+
+start.addEventListener("click", startQuiz)
+let timer;
+
+function startQuiz(){
+    start.style.display = "none";
+    counterRender();
+    timer = setInterval(counterRender, 1000)
+    progressRender();
+    questionRender();
+    quiz.style.display = "block";
+}
+
+function scoreRender(){
+    ServiceWorkerContainer.style.display = "block";
+    let scorePercent = Math.round(score/questions.length *100);
+    let img = ( scorePercent >= 70) ? "smile.png":
+              ( scorePercent >= 50) ? "medium.png": "sad.jpeg";
+        scoreContainer.innerHTML = "<img src" + img +"><p> scorePercent + % </p>"
+}
